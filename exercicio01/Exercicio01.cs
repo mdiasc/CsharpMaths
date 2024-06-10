bef64a0c525c5ea81293deaa6ced3567db1d2b82
milena <milena-.dias@live.com>
2024-06-10 10:51:13 -0300

    Operadores matemáticos

---
+++ exercicio01/Exercicio01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+class Exercicio01 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Math.Max(5,10));
+        Console.WriteLine(Math.Min(5,10));
+        Console.WriteLine(Math.Sqrt(64));
+        Console.WriteLine(Math.Abs(-4.7));
+        Console.WriteLine(Math.Round(9.99));
+    }
+}
